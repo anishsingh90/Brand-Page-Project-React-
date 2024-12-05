f0ce3010b74ecafdfea93d751a54d751f29cf773
ANISH-SINGH <anishraaz90@gmail.com>
2024-12-05 23:59:22 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+const App = () =>{
+  return (
+  <div>
+    <nav>
+      <div className="logo">
+        <img src="/Image/brand_logo.png" alt="logo"/>
+      </div>
+      <ul>
+        <li href="#">Menu</li>
+        <li href="#">Location</li>
+        <li href="#">About</li>
+        <li href="#">Contact</li>
+      </ul>
+
+      <button>Login</button>
+    </nav>
+  </div>
+  );
+};
+
+
+export default App;
