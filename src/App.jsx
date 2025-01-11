17731fbd7b01cf999fbc7be88a6dd0527dafeefe
ANISH-SINGH <anishraaz90@gmail.com>
2025-01-11 23:12:37 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,15 @@
-const App = () =>{
-  return (
-  <div>
-    <nav>
-      <div className="logo">
-        <img src="/Image/brand_logo.png" alt="logo"/>
-      </div>
-      <ul>
-        <li href="#">Menu</li>
-        <li href="#">Location</li>
-        <li href="#">About</li>
-        <li href="#">Contact</li>
-      </ul>
-      <button>Login</button>
-    </nav>
-  </div>
+import './App.css';
+import HeroSection from './components/Hero';
+import Navigation from './components/navigation';
+const App = () => {
+  return(
+    <>
+    <div>
+      <Navigation/>
+      <HeroSection/>
+    </div>
+    </>
   );
-};
-
+}
 
 export default App;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,37 @@
+import './Hero.css';
+const HeroSection = () =>{
+    return (
+        <>
+        <main className="hero">
+            <div className="hero-content">
+                <h1>YOUR FEET DESERVE THE BEST</h1>
+                <p>YOUR FEET DESERVE THE BEST AND WE'RE HERE 
+                    TO HELP YOU WITH OUR SHOES. YOUR FEET DESERVE 
+                    THE BEST AND WE'RE HERE TO HELP YOU WITH OUR 
+                    SHOES.
+                </p>
+
+                {/* button  */}
+                <div className="hero-button">
+                    <button>Shop Now</button>
+                    <button>Category</button>
+                </div>
+
+              <div className="shopping">
+                <p>Also Available On</p>
+                <div className="brand-icons">
+                    <img src="/Image/amazon.png" alt="amazon-logo"/>
+                    <img src="/Image/flipkart.png" alt="flipkar-logo"/>
+                </div>
+            </div>  
+
+            </div>
+            <div className="hero-image">
+            <img src="/Image/hero-image.png" alt="hero-image"/>
+            </div>
+        </main>
+        </>
+    );
+}
+
+export default HeroSection;
